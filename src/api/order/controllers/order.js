e9142c48639fa14257d74ec6d1f28ebfe199d02c
Alek30k <alemank23k@gmail.com>
2025-05-23 18:54:05 -0300

    last commit

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,38 @@
-'use strict';
+"use strict";
+
+//@ts-ignore
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+// module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ stripe }) => ({
+  async create(ctx) {
+    //@ts-ignore
+
+    const { products } = ctx.request.body;
+
+    try {
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await stripe
+            .service("api::product.product")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "ARS",
+              product_data: {
+                name: item.productName,
+              },
+            },
+          };
+        })
+      );
+    } catch (error) {}
+  },
+}));
