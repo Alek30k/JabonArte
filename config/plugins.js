a773ec8fdd1bb003039cdc287da5af1098a6687e
Alek30k <alemank23k@gmail.com>
2025-05-26 18:28:40 -0300

    last commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,25 @@
+// module.exports = ({ env }) => ({
+//   "strapi-neon-tech-db-branches": {
+//     enabled: false,
+//   },
+// });
+
 module.exports = ({ env }) => ({
   "strapi-neon-tech-db-branches": {
     enabled: false,
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
   },
 });
