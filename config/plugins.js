93fde7eea95aa0e8ac3976dc489ea90166be89d2
Alek30k <alemank23k@gmail.com>
2025-05-26 15:22:09 -0300

    last commit-api

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,12 @@
 module.exports = {
-  //   "strapi-neon-tech-db-branches": {
-  //     enabled: true,
-  //     config: {
-  //       neonApiKey:
-  //         "napi_gzqlz98ek3mlr1r2q1n7ho5iqiz2xb7g8bi1pwcvhyhxkzwma0dy9ax78i52wu12", // get it from here: https://console.neon.tech/app/settings/api-keys
-  //       neonProjectName: "ecommerce", // the neon project under wich your DB runs
-  //       neonRole: "neondb_owner", // create it manually under roles for your project first
-  //       gitBranch: "main", // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
-  //     },
-  //   },
+  "strapi-neon-tech-db-branches": {
+    enabled: true,
+    config: {
+      neonApiKey:
+        "napi_b2s9s8kx7gjz5yx5wlypd1nc18j8swqqeeg948da5pgm2jpdsx24i5fhe2aofmu2", // get it from here: https://console.neon.tech/app/settings/api-keys
+      neonProjectName: "ecommerce", // the neon project under wich your DB runs
+      neonRole: "neondb_owner", // create it manually under roles for your project first
+      gitBranch: "main", // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
+    },
+  },
 };
